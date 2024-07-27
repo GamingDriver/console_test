f5567a18d33df40426d0f1e18da474148179955c
GamingDriver <yotov180@gmail.com>
2024-07-27 14:08:28 +0200

    initial commit

---
+++ console_test/utilities.c
@@ -0,0 +1,13 @@
+#include "utilities.h"
+
+
+uint16_t mysum(uint16_t n) 
+{
+	uint16_t s = 0;
+
+	for(uint16_t i = 0; i<= n; i++)
+	{
+		s += i;
+	}
+	return s;
+}
---
+++ console_test/utilities.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint16_t mysum(uint16_t n);
+
+#ifdef __cplusplus
+}
+#endif
